e74741bab67a6893e6440d8a698cfb9be8be1adc
Steffen Aichele <steffen.aichele@icloud.com>
2026-04-01 13:41:44 +0200

    fix: main as scroll-container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import CVSection from "./components/CVSection";
 
 export default function Home() {
 	return (
-		<main className="h-dvh overflow-y-scroll bg-(--foreground) text-(--text-primary)">
+		<main className="bg-(--foreground) text-(--text-primary)">
 			<HeroSection />
 			<CVSection />
 			<ProjectsSection />
