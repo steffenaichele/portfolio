76b60560ddbac9bacba861cd131cf95423627099
Steffen Aichele <steffen.aichele@icloud.com>
2026-04-08 22:34:53 +0200

    feat: add Vercel Analytics and Speed Insights components; update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./styles/globals.css";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -59,6 +61,8 @@ export default function RootLayout({
 					{children}
 				</main>
 				<Footer />
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
