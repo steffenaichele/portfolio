756b1881daeafae4a5c521172e05cd4ca307791c
Steffen Aichele <steffen.aichele@icloud.com>
2026-04-13 14:19:34 +0200

    style: update padding

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -40,9 +40,9 @@ const variantClasses: Record<Variant, string> = {
 };
 
 const contentClasses: Record<ContentType, string> = {
-	text: "px-5",
+	text: "px-4",
 	iconOnly: "px-4",
-	iconRight: "pr-4 pl-5 gap-3",
+	iconRight: "pr-4 pl-4 gap-3",
 };
 
 const Button = ({
