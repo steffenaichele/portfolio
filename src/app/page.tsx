6e42b3dcf377aecd76c946804d03bdd065bb4155
Steffen Aichele <steffen.aichele@icloud.com>
2026-04-16 13:39:09 +0200

    refactorr: remove HeroSection.tsx, add claude.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import HeroSection from "./components/HeroSection";
 import ProjectsSection from "./components/ProjectsSection";
 import CVSection from "./components/CVSection";
 
 export default function Home() {
 	return (
 		<>
-			<HeroSection />
+			<section className="px-8 pt-100 justify-center flex flex-col gap-3">
+				<h1 className="p-lg text-(--color-text-primary)">
+					Hi, ich bin Steffen <span aria-label="Peace sign">✌🏻</span>
+				</h1>
+				<p className="p-lg text-(--color-text-tertiary)">
+					Ich bin UX/UI Designer und Web Entwickler aus Schwäbisch
+					Gmünd.
+				</p>
+			</section>
 			<ProjectsSection />
 			<CVSection />
 		</>
