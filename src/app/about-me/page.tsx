40fcd9d7f2e1c35f4784452aa70b4cf619b8a314
Steffen Aichele <steffen.aichele@icloud.com>
2026-04-21 15:42:12 +0200

    feat: update header styles by removing background color variable; refine About Me section typography

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -9,8 +9,8 @@ export const metadata: Metadata = {
 export default function AboutMe() {
 	return (
 		<section className="pt-50 px-5 flex flex-col gap-24">
-			<h1 className="5xl-regular">About Me</h1>
-            <p className="md-regular">hello:)</p>
+			<h1 className="text-4xl text-(--color-text-primary)">About Me</h1>
+            <p className="text-(--color-text-primary)">hello:)</p>
 		</section>
 	);
 }
