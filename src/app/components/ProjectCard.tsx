f9efa4bbfc07c2af417aeac17defe7f7d83959c7
Steffen Aichele <steffen.aichele@icloud.com>
2026-05-20 15:45:30 +0200

    refactor: update all imports to use content.ts

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Project } from "@/app/data/projects";
+import { Project } from "@/app/data/content";
 
 type Props = {
 	project: Project;
